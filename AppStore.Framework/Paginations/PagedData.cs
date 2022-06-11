e8dd8813c17b7324538fab89d3199b58b77abd55
MohammadAb1075 <mabdollah1375gmail.com>
2022-06-11 13:58:25 -0700

    .

--- AppStore.Framework/Paginations/PagedData.cs
+++ AppStore.Framework/Paginations/PagedData.cs
@@ -6,3 +6,21 @@
         public PageInfo? PageInfo { get; set; }
     }
 }
+//namespace AppStore.Framework.Paginations
+//{
+//    public class PagedData<T>
+//    {
+
+//        private readonly List<T>? _data;
+
+//        public IReadOnlyList<T> Data
+//        {
+//            get
+//            {
+//                return _data.AsReadOnly();
+//            }
+//        }
+//        //public List<T> Data { get; set; }
+//        public PageInfo PageInfo { get; set; }
+//    }
+//}
